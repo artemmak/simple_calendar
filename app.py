e9191a7f5b0284f8248f9257683c1c3e41eddea9
Artem Maksakov <arte00arte@gmail.com>
2025-11-20 01:37:31 +0200

    added new api routs: to_new_year, days_between, is_weekend

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from datetime import date
+from _datetime import date, datetime
+import utils
 
-from flask import Flask, Response
+from flask import Flask, render_template_string, request
+
 
 app = Flask(__name__)
 
@@ -27,7 +29,7 @@ HTML_TEMPLATE = """
             box-shadow: 0 4px 16px rgba(0,0,0,0.1);
             text-align: center;
         }
-        .date {
+        .text {
             font-size: 28px;
             font-weight: bold;
         }
@@ -36,7 +38,7 @@ HTML_TEMPLATE = """
 
 <body>
     <div class="card">
-        <div class="date">{{ formatted_date }}</div>
+        <div class="text">{{ text }}</div>
     </div>
 </body>
 </html>
@@ -46,8 +48,32 @@ HTML_TEMPLATE = """
 def index():
     today = date.today()
     formatted_date = today.strftime("%d.%m.%Y")
-    html = HTML_TEMPLATE.replace("{{ formatted_date }}", formatted_date)
-    return Response(html, mimetype="text/html; charset=utf-8")
+    return render_template_string(HTML_TEMPLATE, text=formatted_date)
+
+@app.route("/to_new_year")
+def to_new_year():
+    today = date.today()
+    return render_template_string(
+        HTML_TEMPLATE, text=utils.count_days_to_new_year(today)
+    )
+
+@app.route("/days_between")
+def days_between():
+    first_date_str = request.args.get("first")
+    second_date_str = request.args.get("second")
+    first_date = datetime.strptime(first_date_str, "%d.%m.%Y").date()
+    second_date = datetime.strptime(second_date_str, "%d.%m.%Y").date()
+    days_count = utils.count_days_between(first_date, second_date)
+    return render_template_string(HTML_TEMPLATE, text=days_count)
+
+@app.route("/is_weekend")
+def is_weekend():
+    input_date_str = request.args.get("date")
+    input_date = datetime.strptime(input_date_str, "%d.%m.%Y").date()
+    return render_template_string(
+        HTML_TEMPLATE,text=utils.is_weekend(input_date)
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
